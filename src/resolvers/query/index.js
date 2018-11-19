aff01a6e0a4025d1355d1687f7f9cf60d4e8c27c
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-19 14:35:47 -0700

    added people functionality

--- src/resolvers/query/index.js
+++ src/resolvers/query/index.js
@@ -3,5 +3,6 @@ const packageName = require('./packageName');
 const person = require('./person');
 const film = require('./film');
 const films = require('./films');
+const people = require('./people');
 
-module.exports = { info, packageName, person, film, films };
+module.exports = { info, packageName, person, film, films, people };
