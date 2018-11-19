26af4e62a168dd40733e6cf199656c83c0a5a6e9
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-19 12:59:36 -0700

    added original files

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const Query = require('./resolvers/query');
 
 const resolvers= {
   Query
-}
+};
 
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers
-})
+});
 
 if (require.main === module) {
-  server.start(() => console.log(`Server is running`));
+  server.start(() => console.log('Server is running'));
 }
